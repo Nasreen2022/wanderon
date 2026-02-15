69ec97b4637ec52e4832125c647e0a74fe2bfa33
Nasreen Praveen <nasreenpraveen95@gmail.com>
2026-02-15 15:45:20 +0530

    Home updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
 import './App.css'
-import CallbackSection from './component/CallbackSection'
 import Footer from './component/footer/Footer'
-import HeroVideo from './component/HeroVideo'
-import JourneyFrames from './component/JourneyFrames'
 import Navbar from './component/navbar/Navbar'
-import ReviewsBar from './component/ReviewsBar'
-import VideoBannerSlider from './component/VideoBannerSlider'
-import WhyWanderOn from './component/WhyWanderOn'
-import PromoVideo from './component/PromoVideo'
-import ExperienceBanner from './component/ExperienceBanner'
-import RomanticEscapes from './component/RomanticEscapes'
-import ExploreIndia from './component/ExploreIndia'
-import InternationalTrips from './component/InternationalTrips'
-import UpcomingCommunityTrips from './component/UpcomingCommunityTrips'
-import UpcomingTrips from './component/UpcomingTrips'
+import Home from './component/home/Home'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <HeroVideo/>
-      <ReviewsBar/>
-      <ExperienceBanner/>
-      <PromoVideo/>
-      <VideoBannerSlider/>
-      <UpcomingCommunityTrips/>
-      <InternationalTrips/>
-      <ExploreIndia/>
-      <UpcomingTrips/>
-      <RomanticEscapes/>
-      <JourneyFrames/>
-      <WhyWanderOn/>
-      <CallbackSection/>
+      <Home/>
       <Footer/>
     </>
   )
---
+++ src/component/home/Home.jsx
@@ -0,0 +1,35 @@
+import HeroVideo from "./HeroVideo";
+import ReviewsBar from "./ReviewsBar";
+import ExperienceBanner from "./ExperienceBanner";
+import PromoVideo from "./PromoVideo";
+import VideoBannerSlider from "./VideoBannerSlider";
+import UpcomingCommunityTrips from "./UpcomingCommunityTrips";
+import InternationalTrips from "./InternationalTrips";
+import ExploreIndia from "./ExploreIndia";
+import UpcomingTrips from "./UpcomingTrips";
+import RomanticEscapes from "./RomanticEscapes";
+import JourneyFrames from "./JourneyFrames";
+import WhyWanderOn from "./WhyWanderOn";
+import CallbackSection from "./CallbackSection";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+
+      <HeroVideo />
+      <ReviewsBar />
+      <ExperienceBanner />
+      <PromoVideo />
+      <VideoBannerSlider />
+      <UpcomingCommunityTrips />
+      <InternationalTrips />
+      <ExploreIndia />
+      <UpcomingTrips />
+      <RomanticEscapes />
+      <JourneyFrames />
+      <WhyWanderOn />
+      <CallbackSection />
+
+    </div>
+  );
+}
